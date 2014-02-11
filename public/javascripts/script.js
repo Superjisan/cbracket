8d30d0c182fa9ef04188a598f6e4b250ba68ba3a
Nimit Maru <nimit@yealthy.com>
2014-02-11 18:48:05 -0500

    fixing async issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -33,11 +33,24 @@ Game.prototype.play = function (playFunc) {
 };
 
 Game.prototype.render = function (callback) {
-  $("#"+this.domId+" > .top").html(this.team1.seed + ". " + this.team1.name);
-  $("#"+this.domId+" > .bottom").html(this.team2.seed + ". " + this.team2.name);
-  $("#"+this.domId+" > .bottom").animate({opacity: "1"},400,"swing",function() {
-    $("#"+this.domId+" > .top").animate({opacity: "1"}).complete();
+  $("#"+this.domId+" .top").html(this.team1.seed + ". " + this.team1.name);
+  $("#"+this.domId+" .bottom").html(this.team2.seed + ". " + this.team2.name);
+  
+  var self = this;
+  $("#"+self.domId+" .top").fadeTo(30,1,function() {
+    $("#"+self.domId+" .bottom").fadeTo(30, 1,function() {
+      callback();
+    });
   });
+  
+  // var x = $("#"+this.domId+" > .bottom").fadeOut();
+  // console.log(x);
+  
+  // .animate({color: "#00f"}).complete(function () {
+  //       console.log(' in complete');
+  //       
+    
+  // })
   // $("#"+this.domId+" > .top").slideUp();
   
 };
@@ -59,11 +72,16 @@ Bracket.prototype.getHtml = function () {
 };
 
 Bracket.prototype.play = function(playFunc) {
+  this.games[0] = [];
+  this.games[1] = [];
+  this.games[2] = [];
+  this.games[3] = [];
+  
   var startRound=4;
   var game1, game2, winner1, winnerteam1, winner2, winnerteam2, nextGame;
   
   var q = async.queue(function(game,callback) {
-    game.render(callback);
+    game.game.render(callback);
   },1);
   for(var round=startRound; round >= 0; round--) {
     var arr = this.games[round];
@@ -75,21 +93,16 @@ Bracket.prototype.play = function(playFunc) {
       winner1 = game1.play(playFunc);
       winnerteam1 = game1['team'+winner1];
       
-      // if (winnerteam1.name == "UTEP") {
-      //   debugger;
-      // }
-      
       winner2 = game2.play(playFunc);
       winnerteam2 = game2['team'+winner2];
-      if(winnerteam1.pts_against===1816) console.log('team1 ', round, ", ", winnerteam1);
-      if(winnerteam2.pts_against===1816) console.log('team2 ', round, ", ", winnerteam2);
       nextGame = new Game(round-1, Math.floor(game1.spot/2), game1.division, winnerteam1, winnerteam2);
-      q.push({game: nextGame}, function(err){
-        console.log("finished");
-      });
+
       
       if (this.games[round-1]) {
         this.games[round-1].push(nextGame);
+        q.push({game: nextGame}, function(err){
+          console.log("finished");
+        });
       } else {
         break;
       }
@@ -118,14 +131,13 @@ Bracket.prototype.generateBracketHtml = function (teams) {
       if(i===4) {
         teamleft1 = teams[getDbIndexFromSeed(teamOrder[(j%8)*2], Math.ceil(j/8))];
         teamleft1.seed = teamOrder[((j)%8)*2];
-        // console.log("teamleft1: ", teamOrder[(j%8)*2]);
         
         teamleft2 = teams[getDbIndexFromSeed(teamOrder[(j%8)*2+1], Math.ceil(j/8))];
         teamleft2.seed = teamOrder[((j)%8)*2+1];
-        // console.log("teamleft2: ", teamOrder[(j%8)*2+1]);
-        html += "<div class='b bleft b"+i+"' id='b"+i+"-"+j+"-left'><div class='team top'>"+teamleft1.seed+". "+teamleft1.name+"</div><div class='team bottom'>"+teamleft2.seed+". "+teamleft2.name+"</div></div>";
+
+        html += "<div class='b bleft b"+i+" first_round' id='b"+i+"-"+j+"-left'><div class='team top'>"+teamleft1.seed+". "+teamleft1.name+"</div><div class='team bottom'>"+teamleft2.seed+". "+teamleft2.name+"</div></div>";
         var game = new Game(i, j, Math.ceil((j+1)/8), teamleft1, teamleft2, "b"+i+"-"+j+"-left");
-        // var game = new Game(i, j%(Math.pow(2,i-1)), Math.ceil((j+1)/8), teamleft1, teamleft2,"b"+i+"-"+j+"-left");
+
         this.games[i].push(game);
       } else {
         html += "<div class='b bleft b"+i+"' id='b"+i+"-"+j+"-left'><div class='team top'></div><div class='team bottom'></div></div>";
@@ -142,7 +154,7 @@ Bracket.prototype.generateBracketHtml = function (teams) {
         teamright2.seed = teamOrder[(j%8)*2+1];
         // console.log("teamright2: ", teamOrder[(j%8)*2+1]);
         
-        html += "<div class='b bright b"+i+"' id='b"+i+"-"+j+"-right'><div class='team top'>"+teamright1.seed+". "+teamright1.name+"</div><div class='team bottom'>"+teamright2.seed+". "+teamright2.name+"</div></div>";
+        html += "<div class='b bright b"+i+" first_round' id='b"+i+"-"+j+"-right'><div class='team top'>"+teamright1.seed+". "+teamright1.name+"</div><div class='team bottom'>"+teamright2.seed+". "+teamright2.name+"</div></div>";
         var game = new Game(i, j, Math.ceil((j+1)/8)+2, teamright1, teamright2,"b"+i+"-"+j+"-right");
         // var game = new Game(i, j%(Math.pow(2,i-1)), Math.ceil((j+1)/8)+2, teamright1, teamright2,"b"+i+"-"+j+"-right");
         // if (game.team2.name =="Belmont") { debugger; }
