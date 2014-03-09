5f42eccd79187a60fb22a587b0daeb65f92c847e
Esco Obong <menzoic@gmail.com>
2014-03-09 14:20:12 -0400

    forgot to commit controller and view file :X

---
+++ public-src/javascripts/app/controllers/MyAccountCtrl.js
@@ -0,0 +1,14 @@
+app.controller('MyAccountCtrl', function($scope, $http, $sce){
+  $scope.user = bootstrapData.user;
+  $scope.errorFlash = $sce.trustAsHtml(bootstrapData.errorFlash);
+  console.log(bootstrapData);
+  $scope.submit = function(){
+    $http.post("/account", { user: $scope.user, password: $scope.password }).
+      success(function(data, status, headers, config){
+        $scope.responseText = data.msg;
+      }).
+      error(function(data, status, headers, config){
+        $scope.responseText = data.msg || 'An error occured. Please try again';
+      });
+  };
+});
