d495c60ee3194bd5c5f43de673a32015d8b1e9f9
Esco Obong <menzoic@gmail.com>
2014-03-09 18:29:56 -0400

    update alert message logic

--- public-src/javascripts/app/controllers/ResetPasswordCtrl.js
+++ public-src/javascripts/app/controllers/ResetPasswordCtrl.js
@@ -2,9 +2,11 @@ app.controller('ResetPasswordCtrl', function($scope, $http){
   $scope.submit = function(){
     $http.post("/reset-password", { token: bootstrapData.token, password: $scope.password }).
       success(function(data, status, headers, config){
+        $scope.status = true;
         $scope.responseText = data.msg;
       }).
       error(function(data, status, headers, config){
+        $scope.status = false;
         $scope.responseText = data.msg || 'An error occured. Please try again';
       });
   };
