88b66fa369a33b96c196c130d8ef1a6f6db4ea59
Esco Obong <menzoic@gmail.com>
2014-03-09 18:24:51 -0400

    updated user model with unique index for email. Handled duplicat email error when updating account info

--- public-src/javascripts/app/controllers/MyAccountCtrl.js
+++ public-src/javascripts/app/controllers/MyAccountCtrl.js
@@ -5,9 +5,11 @@ app.controller('MyAccountCtrl', function($scope, $http, $sce){
   $scope.submit = function(){
     $http.post("/account", { user: $scope.user, password: $scope.password }).
       success(function(data, status, headers, config){
+        $scope.status = true;
         $scope.responseText = data.msg;
       }).
       error(function(data, status, headers, config){
+        $scope.status = false;
         $scope.responseText = data.msg || 'An error occured. Please try again';
       });
   };
