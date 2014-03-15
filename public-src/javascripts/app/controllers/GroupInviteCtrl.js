2efe18ce66f05567168beb5b9f5f6210dc27e6de
Esco Obong <menzoic@gmail.com>
2014-03-15 15:29:16 -0400

    add cloudsponge contacts importer

--- public-src/javascripts/app/controllers/GroupInviteCtrl.js
+++ public-src/javascripts/app/controllers/GroupInviteCtrl.js
@@ -2,11 +2,31 @@ app.controller('GroupInviteCtrl', function($scope, $http){
   $scope.groups = bootstrapData.groups || [];
   $scope.group = $scope.groups[0];
 
+  window.csPageOptions = {
+    domain_key:'ZD3J7PVQ24VBGFGKFVUV',
+    textarea_id:"contact_list",
+    afterSubmitContacts: getContacts
+  };
+
+  function getContacts(contacts, source, owner) {
+    $scope.emails = _.map(contacts, function(contact){
+        return contact.primaryEmail();
+    });
+  }
+
   $scope.submit = function() {
-    $http.post("/groups/invite", { group: $scope.group, email: $scope.email }).
+    var emails = $scope.emails || [];
+
+    if ($scope.email) {
+      emails.push($scope.email);
+    }
+
+    $http.post("/groups/invite", { group: $scope.group, emails: emails }).
       success(function(data, status, headers, config){
         $scope.status = true;
         $scope.responseText = data.msg;
+        $scope.email = null;
+        $scope.emails = null;
       }).
       error(function(data, status, headers, config){
         $scope.status = false;
