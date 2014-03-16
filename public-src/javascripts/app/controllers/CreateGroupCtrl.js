4b17279e3c3144f15be55a51044ad355d0017d4e
Esco Obong <menzoic@gmail.com>
2014-03-15 21:12:05 -0400

    add invite flow to create group page

--- public-src/javascripts/app/controllers/CreateGroupCtrl.js
+++ public-src/javascripts/app/controllers/CreateGroupCtrl.js
@@ -1,12 +1,34 @@
-app.controller('CreateGroupCtrl', function($scope, $http){
+app.controller('CreateGroupCtrl', function($scope, $http, invite){
   var brackets = bootstrapData.brackets || [];
   $scope.brackets = brackets;
-  $scope.bracket = $scope.brackets[0];
+  $scope.bracket = '';
+
+  invite.init(setEmails);
+
+  function reset() {
+    $scope.emails = null;
+    $scope.email = null;
+    $scope.name = '';
+    $scope.bracket = '';
+    $scope.createGroupForm.$setPristine();
+  }
+
+  function setEmails(emails) {
+    $scope.emails = emails;
+  }
+
   $scope.submit = function() {
-    $http.post("/groups", { name: $scope.name, bracket: $scope.bracket }).
+    var emails = $scope.emails || [];
+
+    if ($scope.email) {
+      emails.push($scope.email);
+    }
+
+    $http.post("/groups", { name: $scope.name, bracket: $scope.bracket, emails: emails  }).
       success(function(data, status, headers, config){
         $scope.status = true;
         $scope.responseText = data.msg;
+        reset();
       }).
       error(function(data, status, headers, config){
         $scope.status = false;
