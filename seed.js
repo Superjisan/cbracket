5077c6709c1e888c0f6765e1e4b0a48d52fc14cc
Nimit Maru <nimit@yealthy.com>
2014-03-16 21:22:47 -0400

    small update to seed to try to fix db update

--- seed.js
+++ seed.js
@@ -252,7 +252,7 @@ var fileScrape = function(dataInfoObj, callback) {
   },
   function(err, results) {
     console.log('done with all async stuff')
-    // db.once('open', function() {
+    db.once('open', function() {
       db.db.dropCollection('teams', function(err) {
         if (err) console.log(err);
         
@@ -297,7 +297,7 @@ var fileScrape = function(dataInfoObj, callback) {
           }
         }
       });
-    // });
+    });
     // process.exit();
       // results is now equals to: {one: 1, two: 2}
   });
