0f2192b748fbfbb2ff73ed1aac48482f62cb9aca
Nimit Maru <nimit@yealthy.com>
2014-04-01 15:45:50 -0400

    removing brackets from standings that were added after 12:15 when the tourney started

--- routes/index.js
+++ routes/index.js
@@ -547,11 +547,29 @@ exports.admin_stats = function(req,res) {
   });  
 };
 
+
+function objectIdWithTimestamp(timestamp) {
+    // Convert string date to Date object (otherwise assume timestamp is a date)
+    if (typeof(timestamp) == 'string') {
+        timestamp = new Date(timestamp);
+    }
+
+    // Convert date object to hex seconds since Unix epoch
+    var hexSeconds = Math.floor(timestamp/1000).toString(16);
+
+    // Create an ObjectId with that hex timestamp
+    var constructedObjectId = ObjectId(hexSeconds + "0000000000000000");
+
+    return constructedObjectId
+}
+
 exports.standings = function(req,res) {
-  
+  /*
+  db.brackets.find({ _id: { $lt: objectIdWithTimestamp('2014/03/20 12:10') } }, {name:1,score:1, _id:0}).sort({score:-1}).limit(10)
+  */
   async.parallel({
     brackets: function(done) {
-      models.Bracket.find().sort({score: -1}).populate("user_id").limit(100).exec(function(err, brackets) {
+      models.Bracket.find({ _id: { $lt: objectIdWithTimestamp('2014/03/20 12:16') } }).sort({score: -1}).populate("user_id").limit(100).exec(function(err, brackets) {
         done(null, brackets);
       })
     }
